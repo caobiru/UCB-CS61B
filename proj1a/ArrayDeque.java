250de8cd01d23c3194a9be82ad515c888e7dd1cf
caobiru <55034084+caobiru@users.noreply.github.com>
2023-05-17 18:43:35 +0800

    Update ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -5,7 +5,7 @@ public class ArrayDeque<T> {
     private T[] items;
 
     /** Creates an empty linked list deque */
-    public ArrayDeque(){
+    public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
         nextFirst = 3;
@@ -13,62 +13,60 @@ public class ArrayDeque<T> {
     }
 
     /** Add an item to the front of the list */
-    public void addFirst(T item){
-        if (size == items.length){
+    public void addFirst(T item) {
+        if (size == items.length) {
             resize(2);
         }
 
-        size ++;
+        size++;
         items[nextFirst] = item;
 
-        if (nextFirst == 0){
+        if (nextFirst == 0) {
             nextFirst = items.length - 1;
         }
-        else{
-            nextFirst --;
+        else {
+            nextFirst--;
         }
     }
 
     /** Add an item to the back of the list */
-    public void addLast(T item){
-        if (size == items.length){
+    public void addLast(T item) {
+        if (size == items.length) {
             resize(2);
         }
 
-        size ++;
+        size++;
         items[nextLast] = item;
 
-        if (nextLast == items.length - 1){
+        if (nextLast == items.length - 1) {
             nextLast = 0;
         }
-        else{
-            nextLast ++;
+        else {
+            nextLast++;
         }
     }
 
     /** Returns true if deque is empty, false otherwise.*/
     public boolean isEmpty(){
-        if (size == 0){
+        if (size == 0) {
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
 
     /** Returns the number of items in the deque.*/
-    public int size(){
+    public int size() {
         return size;
     }
 
     /** Prints the items in the deque from first to last, separated by a space. */
-    public void printDeque(){
-        if (isEmpty()){
+    public void printDeque() {
+        if (isEmpty()) {
             return;
         }
-        else{
+        else {
             for (int i = nextFirst + 1; i != nextLast; i++) {
-                if(i >= items.length){
+                if(i >= items.length) {
                     i = 0;
                 }
                 System.out.print(items[i]);
@@ -76,67 +74,69 @@ public class ArrayDeque<T> {
         }
     }
 
-    /** Removes and returns the item at the front of the deque. If no such item exists, returns null.*/
-    public T removeFirst(){
-        if (isEmpty()){
+    /** Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null.*/
+    public T removeFirst() {
+        if (isEmpty()) {
             return null;
         }
 
-        if (++nextFirst >= items.length){
+        if (++nextFirst >= items.length) {
             nextFirst = 0;
         }
 
         T firstItem = items[nextFirst];
         items[nextFirst] = null;
-        size --;
+        size--;
 
         /* resize the array if it is too empty*/
-        if(size < items.length / 4 && items.length > 8){
+        if(size < items.length / 4 && items.length > 8) {
             resize(0.5);
         }
         return firstItem;
     }
 
-    /** Removes and returns the item at the back of the deque. If no such item exists, returns null.*/
-    public T removeLast(){
-        if (isEmpty()){
+    /** Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null.*/
+    public T removeLast() {
+        if (isEmpty()) {
             return null;
         }
 
-        if (--nextLast < 0){
+        if (--nextLast < 0) {
             nextFirst = items.length - 1;
         }
 
         T lastItem = items[nextLast];
         items[nextLast] = null;
-        size --;
+        size--;
 
         /* resize the array if it is too empty*/
-        if(size < items.length / 4 && items.length > 8){
+        if(size < items.length / 4 && items.length > 8) {
             resize(0.5);
         }
 
         return lastItem;
     }
 
-    /** Gets the item at the given index. If no such item exists, returns null. Must not alter the deque!*/
-    public T get(int index){
-        if (index < 0 || size == 0 || index >= size){
+    /** Gets the item at the given index. If no such item exists, returns null.*/
+    public T get(int index) {
+        if (index < 0 || size == 0 || index >= size) {
             return null;
         }
         return items[(nextFirst + index + 1) % items.length];
     }
 
     /** Gets the item at the given index with recursion */
-    public T getRecursive(int index){
+    private T getRecursive(int index) {
         return null;
     }
 
     /** Resize the array */
-    public void resize(double refactor){
+    private void resize(double refactor) {
         T[] newArray = (T[]) new Object[(int) (size * refactor)];
         for (int i = nextFirst + 1, j = 0 ; j < size; i++, j++) {
-            if (i >= items.length){
+            if (i >= items.length) {
                 i = 0;
             }
             newArray[j] = items[i];
